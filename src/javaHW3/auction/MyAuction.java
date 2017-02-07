4def23c02cd5d766c5489facbe224281d19d13bd
Stormantonio <stormantonio.che@gmail.com>
2017-02-08 00:58:23 +0200

    JavaHW3

--- src/javaHW3/auction/MyAuction.java
+++ src/javaHW3/auction/MyAuction.java
@@ -8,17 +8,69 @@ import java.util.concurrent.CountDownLatch;
  * Условием начала аукциона является наличие 5 автомобилей в салоне, также 10 покупателей возле магазина.
  */
 public class MyAuction {
-    private static final CountDownLatch START = new CountDownLatch(8);
+    private static final CountDownLatch START = new CountDownLatch(17);
+    private static int count = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        int i = 0;
+        int j = 0;
+        boolean a = true;
+        while (a) {
+            switch ((int) (Math.random() * 2 + 1)) {
+                case 1:
+                    i++;
+                    if (i < 6) {
+                        new Thread(new Car(i)).start();
+                        Thread.sleep(randomTime(2000, 500));
+                        break;
+                    }
+                    if (i >= 6 && j >= 11) {
+                        a = false;
+                        break;
+                    }
+                    break;
+                case 2:
+                    j++;
+                    if (j < 11) {
+                        new Thread(new Buyer(j, randomTime(5000, 200))).start();
+                        Thread.sleep(randomTime(1000, 500));
+                        break;
+                    }
+                    if (i >= 6 && j >= 11) {
+                        a = false;
+                        break;
+                    }
+                    break;
+            }
+        }
+
+        while (START.getCount() > 2) {
+            Thread.sleep(100);
+        }
+
+
+        Thread.sleep(1000);
+        System.out.println("В саллоне 5 автомобилей, возле магазина 10 покупателей.");
+        START.countDown();
+        Thread.sleep(1000);
+        System.out.println("Начался аукцион...");
+        START.countDown();
 
     }
 
-    public static class Car implements Runnable {
+    private static int randomTime(int x, int y) {
+        return (int) (Math.random() * x + y);
+    }
+
+    private static class Car implements Runnable {
         private int carNumber;
 
-        public Car(int carNumber) {
+
+        Car(int carNumber) {
             this.carNumber = carNumber;
+        }
+
+        Car() {
         }
 
         @Override
@@ -32,5 +84,30 @@ public class MyAuction {
             }
         }
     }
-}
 
+    private static class Buyer extends Car implements Runnable {
+        private int buyerNumber;
+        private int buyingTime;
+
+        Buyer(int buyerNumber, int buyingTime) {
+            this.buyerNumber = buyerNumber;
+            this.buyingTime = buyingTime;
+        }
+
+        @Override
+        public void run() {
+            try {
+                System.out.println("Покупатель № " + buyerNumber + " ожидает возле автосалона...");
+                START.countDown();
+                START.await();
+                while (count < 5) {
+                    count++;
+                    Thread.sleep(buyingTime);
+                    System.out.println("Покупатель № " + buyerNumber + " купил автомобиль!");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- src/javaHW3/threads/MyThreads.java
+++ src/javaHW3/threads/MyThreads.java
@@ -1,38 +1,30 @@
 package javaHW3.threads;
 
 import java.util.concurrent.Phaser;
-import java.util.concurrent.Semaphore;
-import java.util.jar.Attributes;
 
 /**
  * Created by Asus on 07.02.2017.
  */
 public class MyThreads {
-    public static int i = 1;
+    private static int i = 1;
+    private static int count = 0;
 
     public static void main(String[] args) throws InterruptedException {
-Thread thread = Thread.currentThread();
-        thread.setPriority(10);
-//        thread.join();
-//        Semaphore semaphore = new Semaphore(2, false);
-
-
-
-//            Thread th1 = new Thread((Runnable) threads(semaphore));
-        threads().arrive();
-      /*  th1.start();
-        th1.join();*/
-//        Thread.currentThread().join();
-//            Thread th  = new Thread(new PhaseThread(semaphore, "Thread ", 5));
-        /*th.start();
-        th.join();*/
-        threads().wait();
-        thread.join();
-
-
+        boolean a = true;
+        while (a) {
+            switch (count) {
+                case 0:
+                    threads().isTerminated();
+                    break;
+                case 3:
+                    threads1();
+                    a = false;
+                    break;
+            }
+        }
     }
 
-    public static Phaser threads() throws InterruptedException {
+    private static Phaser threads() throws InterruptedException {
         Phaser phaser = new Phaser(3);
         new Thread(new PhaseThread(phaser.arrive(), phaser, "Thread ", i)).start();
         new Thread(new PhaseThread(phaser.arriveAndAwaitAdvance(), phaser, "Thread ", ++i)).start();
@@ -40,78 +32,35 @@ Thread thread = Thread.currentThread();
         return phaser;
     }
 
-    public static Phaser threads1() {
+    private static Phaser threads1() {
         Phaser phaser1 = new Phaser(1);
         new Thread(new PhaseThread(phaser1.arrive(), phaser1, "Thread ", 5)).start();
         return phaser1;
     }
-}
 
-class CommonResource {
-    int count = 0;
+    private static class PhaseThread implements Runnable {
+        int commonResources;
+        Phaser phaser;
+        String name;
+        int threadNumber;
 
-    public CommonResource(int count) {
-        this.count = count;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
-    }
-}
-
-class PhaseThread implements Runnable {
-//    Semaphore semaphore;
-    int count = 0;
-    int commonResources;
-    Phaser phaser;
-    String name;
-    int threadNumber;
-
-    CommonResource c = new CommonResource(0);
-
-    public PhaseThread(int commonResources, Phaser phaser, String name, int threadNumber) {
-//        this.semaphore = semaphore;
-        this.commonResources = commonResources;
-        this.phaser = phaser;
-        this.name = name;
-        this.threadNumber = threadNumber;
-    }
-
-    public PhaseThread(String name, int threadNumber) {
-//        this.semaphore = semaphore;
-        this.name = name;
-        this.threadNumber = threadNumber;
-    }
-
-    public void run() {
-        try {
-            int i = 0;
-            System.out.println(name + threadNumber + " started");
-            Thread.sleep(runner());
-            System.out.println(name + threadNumber + " ended");
-            phaser.arriveAndDeregister();
-//            c.setCount(i++);
-            count++;
-            if (count == 2) {
-                Thread.currentThread().join();
-            }
-            phaser.arriveAndAwaitAdvance();
-
-            /*if (c.getCount() == 2) {
-                Phaser p = new Phaser(1);
-                new Thread(new PhaseThread(p.arrive(), p, "Thread ", 123));
-            }*/
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        PhaseThread(int commonResources, Phaser phaser, String name, int threadNumber) {
+            this.commonResources = commonResources;
+            this.phaser = phaser;
+            this.name = name;
+            this.threadNumber = threadNumber;
         }
-    }
 
-    private int runner() {
-        int i = 50000;
-        return i / ((int) (Math.random() * 100 + 20));
+        public void run() {
+            try {
+                System.out.println(name + threadNumber + " started");
+                Thread.sleep((int) (Math.random() * 3000 + 500));
+                count++;
+                phaser.arriveAndDeregister();
+                System.out.println(name + threadNumber + " ended");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
